25f741877d77cd4a93c9e8657a5ddc84f8e1a23b
Amey Noolkar <amey-anil.noolkar@warwick.ac.uk>
2023-04-19 08:33:27 +0100

    [DEV, CHK] Blunder resolved. Working proto.

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -1,4 +1,5 @@
 #External imports
+from matplotlib import colors
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -6,7 +7,7 @@ import pandas as pd
 
 #Internal imports
 from utils.utils import (
-    load_pkl, store_pkl, compare)
+    load_pkl, store_pkl)
 from loader.ETL import (
     generate_tensor_dataset, trajectory2tensors, tensor2trajectory)
 from global_config.global_config import (
@@ -19,11 +20,16 @@ from global_config.global_config import (
     
 def trajectory2views(trajectory, obj_id, footage):
     for frame, [k, x1, y1, x2, y2] in enumerate(trajectory):
+        # Re-index the cameras.
+        k -= 1
+        # Scale down the image size.
+        x1, y1, x2, y2 = [co_ord // SCALE_DOWN_FACTOR for co_ord in  [x1, y1, x2, y2]]
         footage[frame][k][y1:y2, x1:x2] = obj_id
     return footage
 
 
 def display_views(footage, num_cams=NUM_CAMS, width=IMAGE_WIDTH, height=IMAGE_HEIGHT):
+    cmap = colors.LinearSegmentedColormap.from_list('Custom Cmap', [plt.cm.Set3(i) for i in range(plt.cm.Set3.N)], plt.cm.Set3.N)
     num_axes_rows = (num_cams // 4)
     if (num_cams % 4) != 0:
         num_axes_rows += 1
@@ -35,16 +41,15 @@ def display_views(footage, num_cams=NUM_CAMS, width=IMAGE_WIDTH, height=IMAGE_HE
     for frame, view in enumerate(footage):
         for k in range(num_cams):
             if not np.array_equal(view[k], prev_view[k]):
-                ax[k//4][k%4].imshow(view[k])
+                ax[k//4][k%4].imshow(view[k], cmap="Set3", norm=colors.Normalize(vmin=1, vmax=11))
                 prev_view[k] = view[k]
-        # fig.suptitle(f"Frame {frame} / {num_frames} ({round(frame * 100 / num_frames, 2)}%)")
-        plt.pause(0.001)
+        fig.suptitle(f"Frame {frame} / {num_frames} ({round(frame * 100 / num_frames, 2)}%)")
+        plt.pause(0.005)
     print()
 
 
 def project_trajectories(trajectories, num_cams=NUM_CAMS, width=IMAGE_WIDTH, height=IMAGE_HEIGHT):
-    view = np.zeros((num_cams, height, width))
-    footage = [view] * max([len(trajectory) for trajectory in trajectories])
+    footage = [np.zeros((num_cams, height, width)) for _ in range(max([len(trajectory) for trajectory in trajectories]))]
     for obj_index, trajectory in enumerate(trajectories):
         footage = trajectory2views(trajectory, obj_index + 1, footage)
         # print(f"\rProgress: {round(frame * 100 / num_frames, 2)}%", end="")
