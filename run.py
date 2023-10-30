e5769ac96600b90cff5f9aa49a34e753bbd4593f
Amey Noolkar <amey-anil.noolkar@warwick.ac.uk>
2023-10-30 07:01:55 +0000

    [CHK] Training in progress.

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 gpu_server = True
 parallel_objects = (not gpu_server) and False
-selected_gpu = "2"
+selected_gpu = "0"
 
 mode = "new"  # new, load or enhance
-model_name = "mse_giou_diag_const_context_50ep.ml"
+model_name = "mse_giou_diag_var_context_50ep.ml"
 # mse_giou_diag_const_context_50ep.ml
 # mse_giou_diag_var_context_50ep.ml
 
